d72aa868055a72a5b608d4dc2b8d6db23eedbfef
David Beauchamp <dbeauchamp@makermedia.com>
2016-05-27 14:45:43 -0700

    Finished blog and post templates

--- wp-content/themes/MiniMakerFaire/index.php
+++ wp-content/themes/MiniMakerFaire/index.php
@@ -2,81 +2,89 @@
 
 <div class="container">
 
-    <div class="row">
+  <div class="row">
 
-        <div class="col-xs-12">
+    <div class="col-xs-12">
 
-            <?php
+      <?php
 
-                //if this was a search we display a page header with the results count. If there were no results we display the search form.
-                if (is_search()) :
+      //if this was a search we display a page header with the results count. If there were no results we display the search form.
+      if (is_search()) :
 
-                     $total_results = $wp_query->found_posts;
+        $total_results = $wp_query->found_posts;
 
-                     echo "<h2 class='page-header'>" . sprintf( __('%s Search Results for "%s"','devdmbootstrap3'),  $total_results, get_search_query() ) . "</h2>";
+        echo "<h2 class='page-header'>" . sprintf( __('%s Search Results for "%s"','devdmbootstrap3'),  $total_results, get_search_query() ) . "</h2>";
 
-                     if ($total_results == 0) :
-                         get_search_form(true);
-                     endif;
+        if ($total_results == 0) :
+          get_search_form(true);
+        endif;
 
-                endif;
+      endif;
 
-            ?>
+      ?>
 
-                <?php // theloop
-                    if ( have_posts() ) : while ( have_posts() ) : the_post();
+      <?php // theloop
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-                        // single post
-                        if ( is_single() ) : ?>
+          // single post
+          if ( is_single() ) : ?>
 
-                            <div <?php post_class(); ?>>
+            <div <?php post_class(); ?>>
 
-                                <h2 class="page-header"><?php the_title() ;?></h2>
+              <div class="all-posts-btn">
+                <a href="/blog"><i class="fa fa-chevron-left" aria-hidden="true"></i> All blog posts</a>
+              </div>
 
-                                <?php if ( has_post_thumbnail() ) : ?>
-                                    <?php the_post_thumbnail(); ?>
-                                <?php endif; ?>
-                                <?php the_content(); ?>
-                                <?php wp_link_pages(); ?>
-                                <?php comments_template(); ?>
+              <?php if ( has_post_thumbnail() ) : ?>
+                <?php the_post_thumbnail('full', array( 'class' => 'img-responsive post-thumb-hero' )); ?>
+              <?php endif; ?>
 
-                            </div>
-                        <?php
-                        // list of posts
-                        else : ?>
-                           <div <?php post_class(); ?>>
+              <h2 class="page-header"><?php the_title() ;?></h2>
 
-                                <h2 class="page-header">
-                                    <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'devdmbootstrap3' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-                                </h2>
+              <p class="post-author">By <?php the_author(); ?>, <span><?php the_date('M j, Y'); ?></span></p>
 
-                                <?php if ( has_post_thumbnail() ) : ?>
-                                   <?php the_post_thumbnail(); ?>
-                                    <div class="clear"></div>
-                                <?php endif; ?>
-                                <?php the_content(); ?>
-                                <?php wp_link_pages(); ?>
-                                <?php  if ( comments_open() ) : ?>
-                                       <div class="clear"></div>
-                                      <p class="text-right">
-                                          <a class="btn btn-success" href="<?php the_permalink(); ?>#comments"><?php comments_number(__('Leave a Comment','devdmbootstrap3'), __('One Comment','devdmbootstrap3'), '%' . __(' Comments','devdmbootstrap3') );?> <span class="glyphicon glyphicon-comment"></span></a>
-                                      </p>
-                                <?php endif; ?>
-                           </div>
+              <?php the_content(); ?>
+              <?php wp_link_pages(); ?>
+              <?php comments_template(); ?>
 
-                         <?php  endif; ?>
+            </div>
+          <?php
+          // list of posts
+          else : ?>
+            <div <?php post_class(); ?>>
 
-                    <?php endwhile; ?>
-                    <?php posts_nav_link(); ?>
-                    <?php else: ?>
+              <h2 class="page-header">
+                  <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'devdmbootstrap3' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+              </h2>
 
-                        <?php get_404_template(); ?>
+              <?php if ( has_post_thumbnail() ) : ?>
+                <?php the_post_thumbnail(); ?>
+                <div class="clear"></div>
+              <?php endif; ?>
 
-                <?php endif; ?>
+              <?php the_content(); ?>
+              <?php wp_link_pages(); ?>
+              <?php  if ( comments_open() ) : ?>
+                <div class="clear"></div>
+                <p class="text-right">
+                  <a class="btn btn-success" href="<?php the_permalink(); ?>#comments"><?php comments_number(__('Leave a Comment','devdmbootstrap3'), __('One Comment','devdmbootstrap3'), '%' . __(' Comments','devdmbootstrap3') );?> <span class="glyphicon glyphicon-comment"></span></a>
+                </p>
+              <?php endif; ?>
+            </div>
 
-       </div>
+          <?php  endif; ?>
+
+        <?php endwhile; ?>
+        <?php posts_nav_link(); ?>
+        <?php else: ?>
+
+          <?php get_404_template(); ?>
+
+      <?php endif; ?>
 
     </div>
+
+  </div>
 
 </div>
 
