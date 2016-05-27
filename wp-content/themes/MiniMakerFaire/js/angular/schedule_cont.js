d246df59f84d060aa5a66dcd1f4fd31596990220
Alicia Williams <alicia@makermedia.com>
2016-05-27 11:47:55 -0600

    changes to create category drop down filter

--- wp-content/themes/MiniMakerFaire/js/angular/schedule_cont.js
+++ wp-content/themes/MiniMakerFaire/js/angular/schedule_cont.js
@@ -1,13 +1,45 @@
   var scheduleApp = angular.module('scheduleApp', []);
 
-
   scheduleApp.controller('scheduleCtrl', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
     $scope.days = ['Friday','Saturday','Sunday'];
+    $http.get('/wp-content/themes/MiniMakerFaire/categories.json')
+     .then(function successCallback(response) {
+       $scope.catJson = [];
+       angular.forEach(response.data.category,function(catArr){
+          $scope.catJson[catArr.id] = catArr.name.trim();
+       });
+      });
     $http.get('/wp-content/themes/MiniMakerFaire/schedule.json')
      .then(function successCallback(response) {
         $scope.schedType = 'all';
-        $scope.schedStage = 'All';
+        $scope.schedStage = '';
+        $scope.schedTopic = '';
         $scope.schedules = response.data.schedule;
+        $scope.tags = []; //unique list of categories
+
+        /* input categories are a comma sepated list of category id's
+            the below will split these into an array,
+            compare them to the catJson to get the category name,
+            and output an array of category names */
+        angular.forEach($scope.schedules, function(schedule){
+          var categories = [];
+          var catList = schedule.category.split(",");
+          angular.forEach(catList, function(catID){
+            catID = catID.trim();
+            if(catID!=''){
+              var addCat = catID;
+              //look up cat id in the category json file to find the matching category name
+              if(catID in $scope.catJson){
+                addCat = $scope.catJson[catID];
+              }
+              categories.push(addCat);
+              //create a unique list of category names for a filter drop down
+              if ($scope.tags.indexOf(addCat) == -1)
+                $scope.tags.push(addCat);
+            }
+          });
+          schedule.category = categories;
+        })
      }, function errorCallback(error) {
        console.log(error);
      });
@@ -22,6 +54,9 @@
     }
     $scope.setStage = function(stage){
       $scope.schedStage = stage;
+    }
+    $scope.setTagFilter = function (tag) {
+      $scope.schedTopic = tag;
     }
   }]).filter('dayFilter', function($filter) {
     // Create the return function and set the required parameter name to **input**
@@ -59,7 +94,7 @@
   }).filter('stageFilter', function() {
     // Create the return function and set the required parameter name to **input**
     return function(schedules,stage) {
-      if(stage!='All'){
+      if(stage!=''){
         var out = [];
         // Using the angular.forEach method, go through the array of data and perform the operation of figuring out if the language is statically or dynamically typed.
         angular.forEach(schedules, function(schedule) {
@@ -88,4 +123,24 @@
 
       return output;
    };
+});
+
+scheduleApp.filter('catFilter', function(){
+  return function(items, catName) {
+    var filtered = [];
+
+    if (!catName || !items.length) {
+      return items;
+    }
+    items.forEach(function(itemElement, itemIndex) {
+      itemElement.category.forEach(function(categoryElement, categoryIndex) {
+
+        if (categoryElement === catName) {
+          filtered.push(itemElement);
+          return false;
+        }
+      });
+    });
+    return filtered;
+  };
 });
--- wp-content/themes/MiniMakerFaire/page-schedule.php
+++ wp-content/themes/MiniMakerFaire/page-schedule.php
@@ -94,66 +94,77 @@ get_header(); ?>
     </ul>
 
     <div class="row header">
-        <div class="col-lg-1">&nbsp;</div>
-        <div class="col-lg-4">
-          <!--<span ng-click="order('name')">Title</span>
-          <span class="sortorder" ng-show="predicate === 'name'" ng-class="{reverse:reverse}"></span>
-          -->
-          Title
-        </div>
-        <div class="col-lg-1">
-          <!--<span ng-click="order('time_start')">Time</span>
-          <span class="sortorder" ng-show="predicate === 'time_start'" ng-class="{reverse:reverse}"></span>
-          -->
-          Time
-        </div>
-        <div class="col-lg-2">
-          <span class="dropdown">
-            <button class="btn btn-link dropdown-toggle" type="button" id="mtm-dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-              Stage: {{schedStage}}
-              <i class="fa fa-chevron-down" aria-hidden="true"></i>
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="mtm-dropdownMenu">
-              <li>
-                <a ng-click="setStage('All')">All</a>
-              </li>
-              <li ng-repeat="schedule in schedules | unique:'nicename' | orderBy: nicename ">
-                <a ng-click="setStage(schedule.nicename)">{{schedule.nicename}}</a>
-              </li>
-            </ul>
-          </span>
-        </div>
-        <div class="col-lg-2">Type</div>
-        <div class="col-lg-2">
-          Topics
-        </div>
+      <div class="col-lg-1">&nbsp;</div>
+      <div class="col-lg-4">
+        <!--<span ng-click="order('name')">Title</span>
+        <span class="sortorder" ng-show="predicate === 'name'" ng-class="{reverse:reverse}"></span>
+        -->
+        Title
+      </div>
+      <div class="col-lg-1">
+        <!--<span ng-click="order('time_start')">Time</span>
+        <span class="sortorder" ng-show="predicate === 'time_start'" ng-class="{reverse:reverse}"></span>
+        -->
+        Time
+      </div>
+      <div class="col-lg-2">
+        <span class="dropdown">
+          <button class="btn btn-link dropdown-toggle" type="button" id="mtm-dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            Stage: {{schedStage}}
+            <i class="fa fa-chevron-down" aria-hidden="true"></i>
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="mtm-dropdownMenu">
+            <li>
+              <a ng-click="setStage('')">All</a>
+            </li>
+            <li ng-repeat="schedule in schedules | unique:'nicename' | orderBy: nicename ">
+              <a ng-click="setStage(schedule.nicename)">{{schedule.nicename}}</a>
+            </li>
+          </ul>
+        </span>
+      </div>
+      <div class="col-lg-2">Type</div>
+      <div class="col-lg-2">
+        <span class="dropdown">
+          <button class="btn btn-link dropdown-toggle" type="button" id="mtm-dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            Topics: {{schedTopic}}
+            <i class="fa fa-chevron-down" aria-hidden="true"></i>
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="mtm-dropdownMenu">
+            <li>
+              <a ng-click="setTagFilter('')">All</a>
+            </li>
+            <li ng-repeat="tag in tags | orderBy: tag"> <a ng-click="setTagFilter(tag)">{{ tag }}</a></li>
+          </ul>
+        </span>
+      </div>
     </div>
-<div class="tab-content">
-    <div ng-repeat="day in days" id="{{day}}Sched" ng-class="day=='Friday'?'active tab-pane':'tab-pane'">
-      <div ng-repeat="schedule in schedules | dayFilter:day | typeFilter: schedType| stageFilter: schedStage | filter:filterData | orderBy:predicate">
-        <div class="row">
-          <div class="col-lg-1"><img src="{{schedule.thumb_img_url}}" alt="{{schedule.name}}" /></div>
-          <div class="col-lg-4"><h3>{{schedule.name}}</h3>
-            <p class="presenterList">{{schedule.maker_list}}</p>
-          </div>
-          <div class="col-lg-1">{{schedule.time_start | date: "shortTime"}} - <br/>{{schedule.time_end | date: "shortTime"}}<br/></div>
-          <div class="col-lg-2">{{schedule.nicename}}</div>
-          <div class="col-lg-2">
-            <div class="schedType">
-              <img ng-if="schedule.type == 'Demo'" src="<?php echo get_bloginfo('template_directory'); ?>/img/demo.png" alt="Maker Exhibit Demo Topic Icon" class="img-responsive" />
-              <img ng-if="schedule.type == 'Talk'" src="<?php echo get_bloginfo('template_directory'); ?>/img/talk.png" alt="Maker Exhibit Talk Topic Icon" class="img-responsive" />
-              <img ng-if="schedule.type == 'Workshop'" src="<?php echo get_bloginfo('template_directory'); ?>/img/workshop.png" alt="Maker Exhibit Workshop Topic Icon" class="img-responsive" />
-              <img ng-if="schedule.type == 'Performance'" src="<?php echo get_bloginfo('template_directory'); ?>/img/performance.png" alt="Maker Exhibit Performance Topic Icon" class="img-responsive" />
+    <div class="tab-content">
+      <div ng-repeat="day in days" id="{{day}}Sched" ng-class="day=='Friday'?'active tab-pane':'tab-pane'">
+        <div ng-repeat="schedule in schedules | dayFilter:day | typeFilter: schedType | stageFilter: schedStage | catFilter:schedTopic | filter:filterData | orderBy:predicate">
+          <div class="row">
+            <div class="col-lg-1"><img src="{{schedule.thumb_img_url}}" alt="{{schedule.name}}" /></div>
+            <div class="col-lg-4"><h3>{{schedule.name}}</h3>
+              <p class="presenterList">{{schedule.maker_list}}</p>
+            </div>
+            <div class="col-lg-1">{{schedule.time_start | date: "shortTime"}} - <br/>{{schedule.time_end | date: "shortTime"}}<br/></div>
+            <div class="col-lg-2">{{schedule.nicename}}</div>
+            <div class="col-lg-2">
+              <div class="schedType">
+                <img ng-if="schedule.type == 'Demo'" src="<?php echo get_bloginfo('template_directory'); ?>/img/demo.png" alt="Maker Exhibit Demo Topic Icon" class="img-responsive" />
+                <img ng-if="schedule.type == 'Talk'" src="<?php echo get_bloginfo('template_directory'); ?>/img/talk.png" alt="Maker Exhibit Talk Topic Icon" class="img-responsive" />
+                <img ng-if="schedule.type == 'Workshop'" src="<?php echo get_bloginfo('template_directory'); ?>/img/workshop.png" alt="Maker Exhibit Workshop Topic Icon" class="img-responsive" />
+                <img ng-if="schedule.type == 'Performance'" src="<?php echo get_bloginfo('template_directory'); ?>/img/performance.png" alt="Maker Exhibit Performance Topic Icon" class="img-responsive" />
+              </div>
+            </div>
+            <div class="col-lg-2">
+              <span data-ng-repeat="catName in schedule.category">{{catName}}<font ng-show="!$last">, </font></span>
             </div>
           </div>
-          <div class="col-lg-2">{{schedule.category}}</div>
         </div>
       </div>
     </div>
-</div>
   </div>
-
-
 </div>
 
 
