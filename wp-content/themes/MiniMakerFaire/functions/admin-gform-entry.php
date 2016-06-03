bbf5188d8815c8b7c1c19d81119f68fc5628f768
Alicia Williams <alicia@makermedia.com>
2016-06-03 11:24:42 -0600

    action to lock certain master fields by removing the delete icon

---
+++ wp-content/themes/MiniMakerFaire/functions/admin-gform-entry.php
@@ -0,0 +1,24 @@
+<?php
+add_action( 'gform_delete_field_link', 'mf_delete_field_link', 10, 1 );
+function mf_delete_field_link( $delete_field_link ) {
+  //TBD - Put list of locked fields in DB
+  $lockedFields = array('151','16', '22', '27', '96', '98', '101',
+  '105', '160', '161', '217', '234', '158','162', '224',
+  '258', '155', '167', '223', '259', '156', '166', '222', '260', '157',
+  '165', '220', '261', '159', '164', '221', '262', '154', '163', '219',
+  '233', '109', '111', '110', '320', '321', '303','304');
+
+  //find beginning of field id (gfield_delete_{$this->id}')
+  $fieldID = 0;
+  $fieldIDstart = strpos($delete_field_link, 'gfield_delete_');
+  if($fieldIDstart !== false )
+    $fieldIDend   = strpos($delete_field_link, "'",$fieldIDstart);
+  if($fieldIDend !== false && $fieldIDstart !== false)
+    $fieldID = substr($delete_field_link, $fieldIDstart+14, $fieldIDend-$fieldIDstart-14);
+  if(in_array($fieldID,$lockedFields,true)){
+    return '';
+  }else{
+    return $delete_field_link;
+  }
+}
+
