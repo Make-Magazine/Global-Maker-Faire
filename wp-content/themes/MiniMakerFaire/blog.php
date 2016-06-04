168e4162138721eebf1016e998590ba4e1470c90
David Beauchamp <dbeauchamp@makermedia.com>
2016-06-03 18:33:22 -0700

    Fixing the blog post feed pagination.

--- wp-content/themes/MiniMakerFaire/blog.php
+++ wp-content/themes/MiniMakerFaire/blog.php
@@ -6,99 +6,58 @@ get_header(); ?>
 
   <div class="container">
 
-    <?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
+    <?php query_posts('post_type=post&post_status=publish&posts_per_page=9&paged='. get_query_var('paged')); ?>
 
     <?php if( have_posts() ): ?>
 
       <div id="post-<?php get_the_ID(); ?>" <?php post_class(); ?>>
 
-        <?php //Large first post 
-
-          $args = array( 'numberposts' => '1' );
-          $recent_posts = wp_get_recent_posts( $args );
-          foreach( $recent_posts as $recent ){
-            echo '<div class="row">
-                    <div class="recent-post-post first-post col-xs-12">
-                      <article class="recent-post-inner">
-                        <a href="' . get_permalink($recent["ID"]) . '">';
-                        if ( get_the_post_thumbnail($recent['ID']) != '' ) {
-                          $thumb_id = get_post_thumbnail_id($recent['ID']);
-                          $url = wp_get_attachment_url($thumb_id);
-                          echo "<div class='recent-post-img' style='background-image: url(" . $url . ");'></div>";
-                        }
-
-            echo  '       <div class="recent-post-text">
-                            <h4>' . $recent["post_title"] . '</h4>
-                            <p class="recent-post-date">' . mysql2date('M j, Y',  $recent["post_date"]) . '</p>
-                            <p class="recent-post-descripton">' . sanitize_text_field(substr($recent["post_content"], 0 , 300)) . '</p>
-                          </div>
-                        </a>
-                      </article>
-                    </div>
-                  </div>';
-          } ?>
-
           <div class="row">
 
-            <?php //All the other posts after the 1st
+            <?php
+            $i = 0;
+            while( have_posts() ): the_post();
+              echo '<div class="recent-post-post ';
+              if($i == 0){
+                echo 'first-post col-xs-12">';
+              }
+              else{
+                echo 'col-xs-12 col-sm-3">';
+              }
+               
+              echo   '<article class="recent-post-inner">
+                        <a href="' . get_permalink(["ID"]) . '">';
 
-            $args = array( 'offset' => 1 );
-            $recent_posts = wp_get_recent_posts( $args );
-            foreach( $recent_posts as $recent ){
-              echo '<div class="recent-post-post col-xs-12 col-sm-3">
-                      <article class="recent-post-inner">
-                        <a href="' . get_permalink($recent["ID"]) . '">';
-                        if ( get_the_post_thumbnail($recent['ID']) != '' ) {
-                          $thumb_id = get_post_thumbnail_id($recent['ID']);
-                          $url = wp_get_attachment_url($thumb_id);
-                          echo "<div class='recent-post-img' style='background-image: url(" . $url . ");'></div>";
-                        }
+              if ( get_the_post_thumbnail(['ID']) != '' ) {
+                $thumb_id = get_post_thumbnail_id(['ID']);
+                $url = wp_get_attachment_url($thumb_id);
+                echo "<div class='recent-post-img' style='background-image: url(" . $url . ");'></div>";
+              }
 
               echo '      <div class="recent-post-text">
-                            <h4>' . $recent["post_title"] . '</h4>
-                            <p class="recent-post-date">' . mysql2date('M j, Y',  $recent["post_date"]) . '</p>
-                            <p class="recent-post-descripton">' . sanitize_text_field(substr($recent["post_content"], 0 , 150)) . '</p>
+                            <h4>' . get_the_title() . '</h4>
+                            <p class="recent-post-date">' . get_the_time("M j, Y") . '</p>
+                            <p class="recent-post-descripton">' . substr(get_the_excerpt(), 0,150) . '</p>
                           </div>
                         </a>
                       </article>
                     </div>';
-            } ?>
+              $i++;
+            endwhile; ?>
 
           </div>
-
-
-
-          <?php //All other posts 4 column ?>
-          <div class="row">
-
-<!--           foreach( $recent_posts as $recent ){
-            echo '<div class="recent-post-post col-xs-12 col-sm-3">
-                    <article class="recent-post-inner">
-                      <a href="' . get_permalink($recent["ID"]) . '">';
-                      if ( get_the_post_thumbnail($recent['ID']) != '' ) {
-                        $thumb_id = get_post_thumbnail_id($recent['ID']);
-                        $url = wp_get_attachment_url($thumb_id);
-                        echo "<div class='recent-post-img' style='background-image: url(" . $url . ");'></div>";
-                      }
-
-            echo  '     <div class="recent-post-text">
-                          <h4>' . $recent["post_title"] . '</h4>
-                          <p class="recent-post-date">' . mysql2date('M j, Y',  $recent["post_date"]) . '</p>
-                          <p class="recent-post-descripton">' . sanitize_text_field(substr($recent["post_content"], 0 , 150)) . '</p>
-                        </div>
-                      </a>
-                    </article>
-                  </div>';
-          } -->
-
-          </div>    
-
 
         </div> <?php //.blog ?>
 
 
-      <div class="navigation">
-        <span class="newer"><?php previous_posts_link(__('« Newer','example')) ?></span> <span class="older"><?php next_posts_link(__('Older »','example')) ?></span>
+      <div class="navigation bottom15">
+        <span class="newer">
+          <?php previous_posts_link(__('<i class="fa fa-chevron-left" aria-hidden="true"></i> Newer','example')) ?>
+        </span>
+        <span class="older pull-right">
+          <?php next_posts_link(__('Older <i class="fa fa-chevron-right" aria-hidden="true"></i>','example')) ?>
+        </span>
+        <div class="clearfix"></div>
       </div><!-- /.navigation -->
 
     <?php else: ?>
