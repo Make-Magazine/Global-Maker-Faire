4ab76d34c628f0f07e1f7c697b9b31bb45698843
David Beauchamp <dbeauchamp@makermedia.com>
2016-06-29 17:48:38 -0700

    Updating WIMF and posts feed panels.

--- wp-content/themes/MiniMakerFaire/page-home.php
+++ wp-content/themes/MiniMakerFaire/page-home.php
@@ -469,8 +469,9 @@ get_header();
                     </div>
                     <div class="row">
                       <div class="col-md-10 col-md-offset-1">
-                        <p class="text-center">Maker Faire is a gathering of fascinating, curious people who enjoy learning and who love sharing what they can do.  From artists to scientists to engineers to crafters, Maker Faire is a venue to for these “makers” to show hobbies, experiments, projects.</p>
-                        <p class="text-center">We call it the Greatest Show (& Tell) on Earth — a family-friendly showcase of invention, creativity, and resourcefulness. Glimpse the future and get inspired!</p>
+                        <p class="text-center">Maker Faire is a gathering of fascinating, curious people who enjoy learning and who love sharing what they can do. From engineers to artists to scientists to crafters, Maker Faire is a venue to for these “makers” to show hobbies, experiments, projects.</p>
+                        <p class="text-center">We call it the Greatest Show (& Tell) on Earth — a family-friendly showcase of invention, creativity, and resourcefulness.</p>
+                        <p class="text-center">Glimpse the future and get inspired!</p>
                       </div>
                     </div>
                   </div>
