0d02a6d46df245780ccc6d6f9212d15a21663f0b
Rich Haynie <rhaynie@slashdotmedia.com>
2016-08-22 17:58:37 -0600

    more debugging on API's.

--- wp-content/themes/MiniMakerFaire/js/dynamic/newsletter.php
+++ wp-content/themes/MiniMakerFaire/js/dynamic/newsletter.php
@@ -4,8 +4,8 @@ header('Content-type: text/javascript');
 
 $parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
 require_once( $parse_uri[0] . 'wp-load.php' );
- $webapioptions = get_option('gravityformsaddon_gravityformswebapi_settings', array() );
-
+$webapioptions = get_option('gravityformsaddon_gravityformswebapi_settings', array() );
+echo '<!-- '.print_r($webapioptions,true).' -->';
 // Setup the API variables required for posting emails
 //set API keys
 $api_key = $webapioptions['public_key'];
