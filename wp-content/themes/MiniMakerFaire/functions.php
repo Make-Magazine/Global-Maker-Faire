7580a72b63789f30798262ac9fb743708eb4fa7e
Rich Haynie <rhaynie@slashdotmedia.com>
2017-02-16 12:35:34 -0700

    Change to SMTP.

--- wp-content/themes/MiniMakerFaire/functions.php
+++ wp-content/themes/MiniMakerFaire/functions.php
@@ -237,7 +237,35 @@ function enqueue_admin()
 
   add_theme_support( 'automatic-feed-links' );
 
+////////////////////////////////////////////////////////////////////
+// Adds SMTP Settings
+////////////////////////////////////////////////////////////////////
 
+add_action('phpmailer_init', 'send_smtp_email');
+
+function send_smtp_email($phpmailer) {
+
+  // Define that we are sending with SMTP
+  $phpmailer->isSMTP();
+
+  // The hostname of the mail server
+  $phpmailer->Host = "smtp.mandrillapp.com";
+
+  // Use SMTP authentication (true|false)
+  $phpmailer->SMTPAuth = true;
+
+  // SMTP port number - likely to be 25, 465 or 587
+  $phpmailer->Port = "587";
+
+  // Username to use for SMTP authentication
+  $phpmailer->Username = "webmaster@makermedia.com";
+
+  // Password to use for SMTP authentication
+  $phpmailer->Password = "NM4IvbgO5LFJeBfRGP_wMg";
+
+  // Encryption system to use - ssl or tls
+  $phpmailer->SMTPSecure = "";
+}
 ////////////////////////////////////////////////////////////////////
 // Sponsor slider panel from sponsors set in sponsors page
 ////////////////////////////////////////////////////////////////////
