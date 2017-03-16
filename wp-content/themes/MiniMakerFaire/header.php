c94a990c9ec618b99e3c734321ef5013c3513d76
David Beauchamp <dbeauchamp@makermedia.com>
2017-03-16 13:07:48 -0700

    Adding the Google Analytics tracking script

--- wp-content/themes/MiniMakerFaire/header.php
+++ wp-content/themes/MiniMakerFaire/header.php
@@ -49,6 +49,15 @@
 </head>
 <body <?php body_class(); ?>>
 
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-51157-33', 'auto');
+    ga('send', 'pageview');
+  </script>
+
   <div class="flag-banner header-flag"></div>
   <div class="alert alert-danger text-center" role="alert">
 Site Maintenance: Thurs 3/16, 9PM PDT to 2AM PDT. Any changes made to your site during this timeframe will not be saved.</div>
