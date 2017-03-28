44e0317e7cc861597545f599df6eef7f17e5cb1e
Rich Haynie <rhaynie@slashdotmedia.com>
2017-03-28 15:30:02 -0600

    Canonical fix.

--- wp-content/themes/MiniMakerFaire/page-entry.php
+++ wp-content/themes/MiniMakerFaire/page-entry.php
@@ -93,7 +93,7 @@
 
   //Url
   global $wp;
-  $canonical_url ="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] ;
+  $canonical_url = home_url( $wp->request ) . '/' ;
   $sharing_cards->canonical_url = $canonical_url;
 
   $sharing_cards->set_values();
