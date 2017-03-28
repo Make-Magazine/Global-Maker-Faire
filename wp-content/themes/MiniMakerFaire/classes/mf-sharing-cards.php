2303edb0e5300a3cfb7da722fbb7068699a1e3d9
Rich Haynie <rhaynie@slashdotmedia.com>
2017-03-28 14:17:56 -0600

    Remove canonical.

--- wp-content/themes/MiniMakerFaire/classes/mf-sharing-cards.php
+++ wp-content/themes/MiniMakerFaire/classes/mf-sharing-cards.php
@@ -72,7 +72,7 @@ public function set_values ()
     $title = $project_title;
     return $title;
     }
-    global $canonical_url;
+    /*global $canonical_url;
      $canonical_url =$this->canonical_url;
     add_filter( 'wpseo_canonical', 'change_wpseo_canonical' );
     function change_wpseo_canonical( $url ) {
@@ -80,65 +80,10 @@ public function set_values ()
     $url = $canonical_url;
     return $url;
     }
-    
+     * 
+     */
+    add_filter( 'wpseo_canonical', '__return_false' );
     return true;
-//   
-//    add_filter( 'wpseo_twitter_image', 'change_wpseo_twitter_image' );
-//    function change_wpseo_twitter_image( $url ) {
-//    global $project_photo;
-//    $url = $project_photo;
-//    return $url;
-//    }
-//    // Card Type
-//    add_filter( 'wpseo_twitter_card_type', 'change_wpseo_twitter_card_type' );
-//    function change_wpseo_twitter_card_type( $url ) {
-//    $url = 'summary_large_image';
-//    return $url;
-//    }
-//    
-//    
-//    
-//    
-//    // Description
-//    global $project_short;
-//    $project_short = $entry['16'];
-//    function change_wpseo_metadesc( $title ) {
-//    global $project_short;
-//    $text = $project_short;
-//    return $text;
-//    }
-//    add_filter( 'wpseo_metadesc', 'change_wpseo_metadesc' );
-// 
-//    //Website
-//    global $project_website;
-//    $project_website = $entry['27'];
-//    
-//    
-//    //Video
-//    global $project_video;
-//    $project_video = $entry['32'];
-//    
-//    //Title
-//    global $project_title;
-//        $project_title = (string)$entry['151'];
-//    $project_title  = preg_replace('/\v+|\\\[rn]/','<br/>',$project_title);
-//    function change_wpseo_title( $title ) {
-//    global $project_title;
-//    $title = $project_title;
-//    return $title;
-//    }
-//    add_filter( 'wpseo_title', 'change_wpseo_title' );
-//   
-//    //Url
-//    global $canonical_url;
-//    global $wp;
-//     $canonical_url = home_url( $wp->request ) . '/' ;    
-//    function change_wpseo_canonical( $url ) {
-//    global $canonical_url;
-//    $url = $canonical_url;
-//    return $url;
-//    }
-//    add_filter( 'wpseo_canonical', 'change_wpseo_canonical' );
    
 }
 }
