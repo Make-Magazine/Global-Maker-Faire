8cd4305a16106bc7e8537f05f8a099d9130794f4
Alicia Williams <alicia@makermedia.com>
2019-09-05 14:51:29 -0600

    jetpack plugin update

--- wp-content/plugins/jetpack/vendor/autoload.php
+++ wp-content/plugins/jetpack/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit5985f1a10284eb0c27302a6c5f7c4d3e::getLoader();
+return ComposerAutoloaderInitc2ae6a3cccf43daa00760d48eeb407c7::getLoader();
