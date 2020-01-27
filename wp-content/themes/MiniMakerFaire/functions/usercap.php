1df7cae4676446705e083874ddb41997bb2cf0b8
Alicia Williams <alicia@make.co>
2020-01-27 16:17:44 -0700

    allow admins to enter unfiltered html

--- wp-content/themes/MiniMakerFaire/functions/usercap.php
+++ wp-content/themes/MiniMakerFaire/functions/usercap.php
@@ -1,6 +1,16 @@
 <?php
-add_action( 'after_setup_theme', 'mf_theme_setup' );
-function mf_theme_setup(){
-    load_theme_textdomain( 'MiniMakerFaire', get_template_directory() . '/languages' );
+/**
+ * Enable unfiltered_html capability for Admins.
+ *
+ * @param  array  $caps    The user's capabilities.
+ * @param  string $cap     Capability name.
+ * @param  int    $user_id The user ID.
+ * @return array  $caps    The user's capabilities, with 'unfiltered_html' potentially added.
+ */
+function add_unfiltered_html_capability_to_admins( $caps, $cap, $user_id ) {
+	if ( 'unfiltered_html' === $cap && user_can( $user_id, 'administrator' ) ) {
+		$caps = array( 'unfiltered_html' );
+	}
+	return $caps;
 }
-
+add_filter( 'map_meta_cap', 'add_unfiltered_html_capability_to_admins', 1, 3 );
